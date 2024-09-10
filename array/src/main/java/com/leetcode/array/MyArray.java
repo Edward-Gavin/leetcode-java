9ffa4051b57bddb7b9917fc818f6f1236cb931a8
shiwei10 <shiwei10@staff.weibo.com>
2024-09-10 11:15:57 +0800

    feat: remove pom

--- array/src/main/java/com/leetcode/array/MyArray.java
+++ array/src/main/java/com/leetcode/array/MyArray.java
@@ -5,4 +5,10 @@ package com.leetcode.array;
  * @Date: 2024/9/10 11:00
  */
 public class MyArray {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
 }
