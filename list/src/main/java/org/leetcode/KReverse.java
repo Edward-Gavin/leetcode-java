2737a5177039ae97c48d2c99e4debd6cfdb14492
shiwei10 <shiwei10@staff.weibo.com>
2024-09-11 16:41:30 +0800

    feat: add k reverse

--- list/src/main/java/org/leetcode/KReverse.java
+++ list/src/main/java/org/leetcode/KReverse.java
@@ -5,7 +5,51 @@ package org.leetcode;
  * @Date: 2024/9/10 14:36
  */
 public class KReverse {
-    public static ListNode reverse(ListNode head, int k) {
+    public static ListNode kReserve(ListNode head, int k) {
+
+        if (head == null || k == 1){
+            return head;
+        }
+
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+
+        ListNode start = dummy;
+        ListNode end = head;
+        int count = 0;
+        while (end != null) {
+            count++;
+            if (count % k == 0) {
+                start = reverse(start, end.next);
+                end = start.next;
+            } else {
+                end = end.next;
+            }
+        }
+        return dummy.next;
+
+    }
+
+    public static ListNode reverse(ListNode start, ListNode end){
+        ListNode cur = start.next;
+        ListNode pre = start;
+        ListNode first = cur;
+
+        while (cur != end) {
+            ListNode temp = start.next;
+            start.next = pre;
+            pre = cur;
+            cur = temp;
+        }
+
+        start.next = pre;
+        first.next = cur;
+
+        return first;
+    }
+
+
+    public static ListNode reverse(ListNode head) {
         // 头插法
         ListNode curNode = head;
         ListNode nextNode = null;
@@ -25,7 +69,7 @@ public class KReverse {
         ListNode l3 = new ListNode(3, null);
         ListNode l2 = new ListNode(2, l3);
         ListNode l1 = new ListNode(1, l2);
-        ListNode reverse = reverse(l1, 3);
+        ListNode reverse = reverse(l1);
         while (reverse != null) {
             System.out.println(reverse.val);
             reverse = reverse.next;
