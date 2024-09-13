7029567712341f3c7ce49eabdd5983d4d261278a
shiwei10 <shiwei10@staff.weibo.com>
2024-09-13 13:36:27 +0800

    feat: delete last nth node from list

---
+++ list/src/main/java/org/leetcode/DeleteLastNNode.java
@@ -0,0 +1,44 @@
+package org.leetcode;
+
+/**
+ * @Author: shiwei10
+ * @Date: 2024/9/13 12:51
+ */
+public class DeleteLastNNode {
+    public static ListNode removeNthFromEnd(ListNode head, int n) {
+
+        if (head == null) {
+            return null;
+        }
+
+        ListNode dummy = new ListNode(-1,head);
+        ListNode slow = dummy;
+        ListNode fast = dummy;
+
+        for (int i = 0; i < n; i++) {
+            fast = fast.next;
+        }
+
+        while (fast.next != null) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+
+        slow.next = slow.next.next;
+
+        return dummy.next;
+    }
+
+    public static void main(String[] args) {
+        ListNode l5 = new ListNode(5, null);
+        ListNode l4 = new ListNode(4, l5);
+        ListNode l3 = new ListNode(3, l4);
+        ListNode l2 = new ListNode(2, l3);
+        ListNode l1 = new ListNode(1, l2);
+        ListNode reverse = removeNthFromEnd(l1, 2);
+        while (reverse != null) {
+            System.out.println(reverse.val);
+            reverse = reverse.next;
+        }
+    }
+}
