74944485956c0f9d4344c51c35b8faaa3e62ba66
shiwei10 <shiwei10@staff.weibo.com>
2024-09-19 20:10:54 +0800

    feat: add binary search

--- binary_search/src/main/java/com/leetcode/middle/NSearch.java
+++ binary_search/src/main/java/com/leetcode/middle/NSearch.java
@@ -5,13 +5,48 @@ package com.leetcode.middle;
  * @Date: 2024/9/19 19:44
  */
 public class NSearch {
-    public int search(int[] nums, int target){
+    public int search(int[] nums, int target) {
 
+        int length = nums.length;
 
+        // 找到旋转的点
+        int position = 0;
+        for (int i = 1; i < length; i++) {
+            if (nums[i-1] > nums[i]) {
+                position = i;
+                break;
+            }
+        }
 
+        // 左侧二分
+        int lPos = binarySearch(nums, 0, position, target);
 
+        // 右侧二分
+        int rPos = binarySearch(nums, position, length, target);
 
+        if (lPos == -1 && rPos == -1) {
+            return -1;
+        }
 
+        if (lPos == -1) {
+            return rPos;
+        } else {
+            return lPos;
+        }
+
+    }
+
+    public static int binarySearch(int[] nums, int left, int right, int target) {
+        while (left < right) {
+            int mid = (left + right) >> 1;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
         return -1;
     }
 }
