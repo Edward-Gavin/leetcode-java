b46952f125144773ad81419e4ef922afe3211a0f
shiwei10 <shiwei10@staff.weibo.com>
2024-09-27 17:50:16 +0800

    feat: add isValidBST

--- tree/src/main/java/com/leetcode/middle/IsValidBST.java
+++ tree/src/main/java/com/leetcode/middle/IsValidBST.java
@@ -1,6 +1,31 @@
-package com.leetcode.middle;/**
+package com.leetcode.middle;
+
+import com.leetcode.TreeNode;
+
+/**
  * @Author: shiwei10
  * @Date: 2024/9/26 19:01
  */
 public class IsValidBST {
+
+    Integer pre = Integer.MIN_VALUE;
+
+
+    public boolean isValidBST(TreeNode root) {
+
+        if (root == null) {
+            return false;
+        }
+
+        if (!isValidBST(root.left)){
+            return false;
+        }
+        if (root.val < pre) {
+            return true;
+        }
+
+        pre = root.val;
+
+        return isValidBST(root.right);
+    }
 }
