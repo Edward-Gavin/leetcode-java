0ab55bbc1ec90d2f106548cddde4239d02037c74
shiwei10 <shiwei10@staff.weibo.com>
2024-09-27 19:59:55 +0800

    feat: add swap pairs list

--- list/src/main/java/org/leetcode/review/swapPairs.java
+++ list/src/main/java/org/leetcode/review/swapPairs.java
@@ -1,6 +1,21 @@
-package org.leetcode.review;/**
+package org.leetcode.review;
+
+import org.leetcode.ListNode;
+
+/**
  * @Author: shiwei10
  * @Date: 2024/9/27 18:02
  */
 public class swapPairs {
+    public ListNode swapPairs(ListNode head) {
+
+        ListNode dummy = new ListNode(-1, head);
+
+
+
+
+
+
+        return dummy.next;
+    }
 }
