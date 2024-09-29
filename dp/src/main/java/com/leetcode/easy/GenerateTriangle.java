319bdf6ebb692f571adb8e1e2f8523524705f520
shiwei10 <shiwei10@staff.weibo.com>
2024-09-29 16:32:26 +0800

    feat: add dp generate triangle

---
+++ dp/src/main/java/com/leetcode/easy/GenerateTriangle.java
@@ -0,0 +1,33 @@
+package com.leetcode.easy;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @Author: shiwei10
+ * @Date: 2024/9/29 15:35
+ */
+public class GenerateTriangle {
+
+    public List<List<Integer>> res = new ArrayList<>();
+
+    public List<List<Integer>> generate(int numRows) {
+        Integer[][] dp = new Integer[numRows][];
+
+        for (int i = 0; i < numRows; i++) {
+            dp[i] = new Integer[i + 1];
+            dp[i][0] = dp[i][i] = 1;
+            for (int j = 1; j < i; j++) {
+                dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
+            }
+        }
+
+        for (Integer[] ints : dp) {
+            res.add(Arrays.asList(ints));
+        }
+
+        return res;
+    }
+
+}
---
+++ dp/src/main/java/com/leetcode/easy/GenerateTriangleV2.java
@@ -0,0 +1,6 @@
+package com.leetcode.easy;/**
+ * @Author: shiwei10
+ * @Date: 2024/9/29 16:23
+ */
+public class GenerateTriangleV2 {
+}
