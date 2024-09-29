e09b66cc91a5003564170dee3e1a63acb261fe04
shiwei10 <shiwei10@staff.weibo.com>
2024-09-29 14:16:38 +0800

    feat: add double pointer sort color

--- skill/src/main/java/com/leetcode/middle/SortColor.java
+++ skill/src/main/java/com/leetcode/middle/SortColor.java
@@ -3,6 +3,7 @@ package com.leetcode.middle;
 /**
  * @author: shiwei10
  * @create: 2024-09-28 20:45
+ * @Description: 75. 颜色分类
  */
 public class SortColor {
     public void sortColor(int[] nums) {
@@ -12,21 +13,44 @@ public class SortColor {
 
         for (int i = 0; i < length; i++) {
             if (nums[i] == 0) {
-                int temp = nums[i];
-                nums[i] = nums[ptr];
-                nums[ptr] = temp;
+               swap(nums, i, ptr);
                 ptr++;
             }
         }
 
         for (int i = ptr; i < length; i++) {
             if (nums[i] == 1) {
-                int temp = nums[i];
-                nums[i] = nums[ptr];
-                nums[ptr] = temp;
+                swap(nums, i, ptr);
                 ptr++;
             }
         }
 
+    }
+
+    public void sortColor2(int[] nums) {
+
+        int length = nums.length;
+        int left = 0;
+        int right = length - 1;
+        int cur = 0;
+
+        while (cur <= right) {
+            if (nums[cur] == 0) {
+                swap(nums, cur, left);
+                cur++;
+                left++;
+            } else if (nums[cur] == 1) {
+                cur ++;
+            } else {
+                swap(nums, cur, right);
+                right--;
+            }
+        }
+    }
+
+    public static void swap(int[] nums, int left, int right) {
+        int temp = nums[left];
+        nums[left] = nums[right];
+        nums[right] = temp;
     }
 }
