f3fa78441fd43fb7315b6b7e9865fc1c77c2e0fb
Edward-Gavin <wsw_it_3325@163.com>
2024-10-08 22:57:16 +0800

    feat: add product except self of array

---
+++ array/src/main/java/com/leetcode/middle/ProductExceptSelf.java
@@ -0,0 +1,53 @@
+package com.leetcode.middle;
+
+/**
+ * @author: shiwei10
+ * @create: 2024-10-08 22:37
+ */
+public class ProductExceptSelf {
+    public int[] productExceptSelf(int[] nums) {
+        int length = nums.length;
+        int[] res = new int[length];
+
+
+        for (int i = 0; i < length; i++) {
+            int total = 1;
+            for (int j = 0; j < length; j++) {
+                if (i != j) {
+                    total *= nums[j];
+                }
+            }
+            res[i] = total;
+        }
+        return res;
+    }
+
+    public int[] productExceptSelfV2(int[] nums) {
+
+        int length = nums.length;
+        int[] res = new int[length];
+
+        int[] pre = new int[length];
+
+        int[] post = new int[length];
+
+        pre[0] = 1;
+        for (int i = 1; i < length; i++) {
+            pre[i] = pre[i - 1] * nums[i - 1];
+        }
+
+        post[length - 1] = 1;
+
+        for (int i = length - 2; i >= 0; i--) {
+            post[i] = post[i + 1] * nums[i + 1];
+        }
+
+        for (int i = 0; i < length; i++) {
+            res[i] = pre[i] * post[i];
+        }
+
+        return res;
+    }
+
+
+}
