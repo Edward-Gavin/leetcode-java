83e7c8848e62bbb8394d974f13e95f9a4a136975
shiwei10 <shiwei10@staff.weibo.com>
2024-10-09 10:34:01 +0800

    feat: add first miss positive number

--- array/src/main/java/com/leetcode/hard/FirstMissingPositive.java
+++ array/src/main/java/com/leetcode/hard/FirstMissingPositive.java
@@ -4,11 +4,18 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- * @author: shiwei10
- * @create: 2024-10-08 23:01
+ * @Author: shiwei10
+ * @Create: 2024-10-08 23:01
+ * @Description: 41. 数组中缺失的第一个正数
  */
 public class FirstMissingPositive {
-    public int firstMissingPositive(int[] nums) {
+    /**
+     * 引入hash表
+     * 时间复杂度O(n)，空间复杂度O(n)
+     * @param nums
+     * @return
+     */
+    public int firstMissingPositiveV1(int[] nums) {
 
         Map<Integer, Integer> map = new HashMap<>();
 
@@ -23,5 +30,38 @@ public class FirstMissingPositive {
         }
 
         return  nums.length + 1;
+    }
+
+    /**
+     * 原地hash，把数组当作hash表，按序分别找到位置，如果不在位置上，则交换
+     * 时间复杂度O(n)，空间复杂度O(1)
+     * @param nums
+     * @return
+     */
+    public int firstMissingPositiveV2(int[] nums){
+        if (nums == null || nums.length == 0) {
+            return 1;
+        }
+
+        int length = nums.length;
+        for (int i = 0; i < length; i++) {
+            while (nums[i] > 0 && nums[i] <= length && nums[i] != nums[nums[i] - 1]) {
+                swap(nums, i, nums[i] - 1);
+            }
+        }
+
+        for (int i = 0; i < length; i++) {
+            if (nums[i] != i + 1) {
+                return i + 1;
+            }
+        }
+
+        return length + 1;
+    }
+
+    public void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
 }
