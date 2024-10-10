b635f8fc534a065ea3e39391c67d53a6e0c31805
shiwei10 <shiwei10@staff.weibo.com>
2024-10-10 11:37:09 +0800

    feat: add sliding window max number

---
+++ string/src/main/java/com/leetcode/middle/LongestPalindrome.java
@@ -0,0 +1,13 @@
+package com.leetcode.middle;
+
+/**
+ * @Author: shiwei10
+ * @Date: 2024/10/10 10:28
+ */
+public class LongestPalindrome {
+    public String longestPalindrome(String s) {
+
+
+        return "";
+    }
+}
--- window/src/main/java/com/leetcode/hard/MaxSlidingWindow.java
+++ window/src/main/java/com/leetcode/hard/MaxSlidingWindow.java
@@ -1,6 +1,7 @@
 package com.leetcode.hard;
 
 import java.util.Comparator;
+import java.util.LinkedList;
 import java.util.PriorityQueue;
 
 /**
@@ -8,12 +9,14 @@ import java.util.PriorityQueue;
  * @Date: 2024/10/9 20:00
  */
 public class MaxSlidingWindow {
+
+    // 暴力法
     public int[] maxSlidingWindow(int[] nums, int k) {
 
         int length = nums.length;
         int[] res = new int[length + 1 - k];
 
-        int left = 0, right = k-1;
+        int left = 0, right = k - 1;
 
         while (right < length) {
             res[left] = maxNumberWindow(nums, left, right);
@@ -63,12 +66,79 @@ public class MaxSlidingWindow {
         return res;
     }
 
-        public static void main(String[] args) {
+    // 双向队列
+    public int[] maxSlidingWindowV2(int[] nums, int k) {
+
+        LinkedList<Integer> queue = new LinkedList<>();
+
+        int len = nums.length;
+        int[] res = new int[len + 1 - k];
+
+        for (int i = 0; i < len; i++) {
+
+            // 如果队列中的数比新来的数小，依次弹出
+            while (!queue.isEmpty() && nums[queue.peekLast()] <= nums[i]) {
+                queue.pollLast();
+            }
+
+            queue.add(i);
+
+            // 队列里存储的数，大于窗口大小，弹出
+            if (queue.peek() <= i - k) {
+                queue.poll();
+            }
+
+            // 保存当前窗口最大值
+            if (i + 1 >= k) {
+                res[i + 1 - k] = nums[queue.peek()];
+            }
+        }
+
+        return res;
+    }
+
+    // 单调栈
+    public int[] maxSlidingWindowV3(int[] nums, int k) {
+        LinkedList<Integer> queue = new LinkedList<>();
+
+        int[] res = new int[nums.length + 1 - k];
+
+
+        // 形成窗口
+        for (int i = 0; i < k; i++) {
+            while (!queue.isEmpty() && queue.peekLast() < nums[i]) {
+                queue.pollLast();
+            }
+            queue.addLast(nums[i]);
+        }
+
+        res[0] = queue.peekFirst();
+
+
+        // 窗口内操作
+        for (int i = k; i < nums.length; i++) {
+            // 删除窗口左边的元素
+            if (queue.peekFirst() == nums[i - k]) {
+                queue.removeFirst();
+            }
+
+            // 保持队列中的元素last最大
+            while (!queue.isEmpty() && queue.peekLast() < nums[i]) {
+                queue.removeLast();
+            }
+
+            queue.addLast(nums[i]);
+            res[i - k + 1] = queue.peekFirst();
+        }
+
+        return res;
+    }
+
+    public static void main(String[] args) {
         int[] nums = new int[]{1, 3, -1, -3, 5, 3, 6, 7};
-        int[] ints = new MaxSlidingWindow().maxSlidingWindow(nums, 3);
+        int[] ints = new MaxSlidingWindow().maxSlidingWindowV2(nums, 3);
         for (int anInt : ints) {
             System.out.println(anInt);
-
         }
 
     }
