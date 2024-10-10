1bba962a0e75f79f7581b0f0572e8cecc92aaec6
shiwei10 <shiwei10@staff.weibo.com>
2024-10-10 11:55:11 +0800

    feat: add longest palindrome

--- string/src/main/java/com/leetcode/middle/LongestPalindrome.java
+++ string/src/main/java/com/leetcode/middle/LongestPalindrome.java
@@ -7,7 +7,44 @@ package com.leetcode.middle;
 public class LongestPalindrome {
     public String longestPalindrome(String s) {
 
+        if (s == null || s.isEmpty()) {
+            return "";
+        }
 
-        return "";
+        int left, right;
+        int maxStart = 0;
+        int maxLen = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            left = i - 1;
+            right = i + 1;
+
+            while (left >= 0 && s.charAt(left) == s.charAt(i)) {
+                left--;
+            }
+
+            while (right < s.length() && s.charAt(right) == s.charAt(i)) {
+                right++;
+            }
+            while (left >= 0 && right < s.length() && s.charAt(right) == s.charAt(left)) {
+                left--;
+                right++;
+            }
+
+            if (right - left > maxLen) {
+                maxLen = right - left;
+                maxStart = left;
+            }
+
+        }
+
+        return s.substring(maxStart + 1, maxStart + maxLen);
+    }
+
+    public static void main(String[] args) {
+        String s = "aba";
+        String s1 = new LongestPalindrome().longestPalindrome(s);
+        System.out.println(s1);
+
     }
 }
