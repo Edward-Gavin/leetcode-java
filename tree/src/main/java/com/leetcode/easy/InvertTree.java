5968214b71cf9d871e9fd88d06a0b620625b8c94
Edward-Gavin <wsw_it_3325@163.com>
2024-10-13 17:31:48 +0800

    feat: add symmetric tree

--- tree/src/main/java/com/leetcode/easy/InvertTree.java
+++ tree/src/main/java/com/leetcode/easy/InvertTree.java
@@ -1,6 +1,8 @@
 package com.leetcode.easy;
 
 import com.leetcode.TreeNode;
+import java.util.LinkedList;
+import java.util.Queue;
 
 /**
  * @Author: shiwei10
@@ -8,7 +10,11 @@ import com.leetcode.TreeNode;
  * @Description: 226. 翻转二叉树
  */
 public class InvertTree {
-
+    /**
+     * 后序遍历，翻转二叉树
+     * @param root
+     * @return
+     */
     public TreeNode invertTree(TreeNode root) {
         if (root == null) {
             return null;
@@ -21,5 +27,41 @@ public class InvertTree {
         root.right = left;
 
         return root;
+    }
+
+    /**
+     * 层次遍历，并翻转二叉树
+     * @param root
+     * @return
+     */
+    public TreeNode invertTreeLevel(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode poll = queue.poll();
+                swapNode(poll);
+                if (poll.left != null) {
+                    queue.add(poll.left);
+                }
+                if (poll.right != null) {
+                    queue.add(poll.right);
+                }
+            }
+        }
+
+        return root;
+    }
+
+    public void swapNode(TreeNode node) {
+        TreeNode temp = node.left;
+        node.left = node.right;
+        node.right = temp;
     }
 }
--- tree/src/main/java/com/leetcode/easy/IsSymmetric.java
+++ tree/src/main/java/com/leetcode/easy/IsSymmetric.java
@@ -9,9 +9,18 @@ import com.leetcode.TreeNode;
  */
 public class IsSymmetric {
     public boolean isSymmetric(TreeNode root) {
-        return check(root, root);
+        // return check(root, root);
+
+        return compare(root.left, root.right);
     }
 
+    /**
+     * 递归法
+     *
+     * @param p
+     * @param q
+     * @return
+     */
     public boolean check(TreeNode p, TreeNode q) {
         if (p == null && q == null) {
             return true;
@@ -21,6 +30,31 @@ public class IsSymmetric {
             return false;
         }
 
-        return p.val ==q.val && check(p.left, q.right) && check(p.right, q.left);
+        return p.val == q.val && check(p.left, q.right) && check(p.right, q.left);
+    }
+
+    /**
+     * 需要收集孩子的信息题目，需要后序遍历，因为遍历顺序是 左右中，最后遍历父节点可以收集左右孩子的信息
+     *
+     * @param left
+     * @param right
+     * @return boolean
+     */
+    public boolean compare(TreeNode left, TreeNode right) {
+        if (left == null && right == null) {
+            return true;
+        }
+
+        if (left == null || right == null) {
+            return false;
+        }
+
+        if (left.val != right.val) {
+            return false;
+        }
+
+        boolean compare = compare(left.left, right.right);
+        boolean compare1 = compare(left.right, right.left);
+        return compare && compare1;
     }
 }
