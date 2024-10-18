160dace298a2863bbab96aa8a923c0572a4c16f7
shiwei10 <shiwei10@staff.weibo.com>
2024-10-18 19:37:33 +0800

    feat: add product except self

--- array/src/main/java/com/leetcode/middle/ProductExceptSelf.java
+++ array/src/main/java/com/leetcode/middle/ProductExceptSelf.java
@@ -6,10 +6,14 @@ package com.leetcode.middle;
  * @description: 238. 除自身以外数组的乘积
  */
 public class ProductExceptSelf {
+    /**
+     * 时间复杂度O(n^2)
+     * @param nums
+     * @return
+     */
     public int[] productExceptSelf(int[] nums) {
         int length = nums.length;
         int[] res = new int[length];
-
 
         for (int i = 0; i < length; i++) {
             int total = 1;
@@ -23,6 +27,11 @@ public class ProductExceptSelf {
         return res;
     }
 
+    /**
+     * 时间复杂度O(n)
+     * @param nums
+     * @return
+     */
     public int[] productExceptSelfV2(int[] nums) {
 
         int length = nums.length;
@@ -50,5 +59,40 @@ public class ProductExceptSelf {
         return res;
     }
 
+    public int[] productExceptSelfV3(int[] nums) {
 
+        int length = nums.length;
+
+        int[] res = new int[length];
+
+        int[] lr = new int[length];
+        int[] rl = new int[length];
+
+        lr[0] = 1;
+        rl[length - 1] = 1;
+
+        for (int i = 1; i < length; i++) {
+            lr[i] = lr[i - 1] * nums[i - 1];
+        }
+
+        for (int i = length - 2; i >= 0; i--) {
+            rl[i] = rl[i + 1] * nums[i + 1];
+        }
+
+        for (int i = 0; i < length; i++) {
+            res[i] = lr[i] * rl[i];
+        }
+
+        return res;
+    }
+
+    public static void main(String[] args){
+
+        int[] input = new int[]{1,2,3,4};
+        int[] ints = new ProductExceptSelf().productExceptSelfV3(input);
+
+        for (int anInt : ints) {
+            System.out.println(anInt);
+        }
+    }
 }
--- flink/src/main/java/com/leetcode/window/Window.java
+++ flink/src/main/java/com/leetcode/window/Window.java
@@ -4,7 +4,6 @@ import org.apache.flink.api.common.functions.CoGroupFunction;
 import org.apache.flink.api.common.functions.JoinFunction;
 import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.streaming.api.datastream.DataStream;
-import org.apache.flink.streaming.api.functions.co.ProcessJoinFunction;
 import org.apache.flink.streaming.api.windowing.assigners.EventTimeSessionWindows;
 import org.apache.flink.streaming.api.windowing.assigners.SlidingProcessingTimeWindows;
 import org.apache.flink.streaming.api.windowing.assigners.TumblingEventTimeWindows;
