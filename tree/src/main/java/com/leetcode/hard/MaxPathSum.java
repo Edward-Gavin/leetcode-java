d769d39ad837456fb33808504e496cbed5aaada3
shiwei10 <shiwei10@staff.weibo.com>
2024-10-21 19:17:51 +0800

    feat: max path sum

--- tree/src/main/java/com/leetcode/hard/MaxPathSum.java
+++ tree/src/main/java/com/leetcode/hard/MaxPathSum.java
@@ -12,20 +12,20 @@ public class MaxPathSum {
 
     public int maxPathSum(TreeNode root) {
 
-        maxPathSumHelp(root);
+        dfs(root);
 
         return max;
     }
 
-    public int maxPathSumHelp(TreeNode node) {
+    public int dfs(TreeNode node) {
 
         if (node == null) {
             return 0;
         }
         // 在左子树中，包含左孩子节点的最大路径和
-        int leftMax = maxPathSumHelp(node.left);
+        int leftMax = dfs(node.left);
         // 在右子树中，包含右孩子节点的最大路径和
-        int rightMax = maxPathSumHelp(node.right);
+        int rightMax = dfs(node.right);
         // 再往上返回的时候，路径是：要么选左 / 要么选右 / 要么只有根节点
         int curMax = Math.max(Math.max(node.val + leftMax, node.val + rightMax), node.val);
         // 但是我们和最大路径和比较的时候，会多一种情况：既有左又有右（但是这种路径是没法往上层递归返回的，因为此路径已经结束了）
