12081d01b61c9f2fed507a56328949e5c0cdd147
shiwei10 <shiwei10@staff.weibo.com>
2024-12-09 11:28:16 +0800

    review: reverse list

--- list/src/main/java/org/leetcode/ReverseI.java
+++ list/src/main/java/org/leetcode/ReverseI.java
@@ -42,24 +42,24 @@ public class ReverseI {
 
 
     public static void main(String[] args) {
-//        ListNode l9 = new ListNode(9, null);
-//        ListNode l8 = new ListNode(8, l9);
-//        ListNode l7 = new ListNode(7, l8);
-//        ListNode l6 = new ListNode(6, l7);
-//        ListNode l5 = new ListNode(5, l6);
-//        ListNode l4 = new ListNode(4, l5);
-//        ListNode l3 = new ListNode(3, l4);
-//        ListNode l2 = new ListNode(2, l3);
-//        ListNode l1 = new ListNode(1, l2);
-//
-//
-//        ReverseI reverseI = new ReverseI();
-//        ListNode listNode = ReverseI.reverseV1(l1);
-//
-//        while (listNode != null) {
-//            System.out.println(listNode.val);
-//            listNode = listNode.next;
-//        }
+        ListNode l9 = new ListNode(9, null);
+        ListNode l8 = new ListNode(8, l9);
+        ListNode l7 = new ListNode(7, l8);
+        ListNode l6 = new ListNode(6, l7);
+        ListNode l5 = new ListNode(5, l6);
+        ListNode l4 = new ListNode(4, l5);
+        ListNode l3 = new ListNode(3, l4);
+        ListNode l2 = new ListNode(2, l3);
+        ListNode l1 = new ListNode(1, l2);
+
+
+        ReverseI reverseI = new ReverseI();
+        ListNode listNode = ReverseI.reverseV1(l1);
+
+        while (listNode != null) {
+            System.out.println(listNode.val);
+            listNode = listNode.next;
+        }
         System.out.println(5 / 2);
         System.out.println(5 % 2);
     }
