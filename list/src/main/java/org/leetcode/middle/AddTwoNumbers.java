c2a2a3f88904a215b24db08c8da27e581e8d4bcc
shiwei10 <shiwei10@staff.weibo.com>
2025-04-02 19:23:10 +0800

    feat: add two number of list

--- list/src/main/java/org/leetcode/middle/AddTwoNumbers.java
+++ list/src/main/java/org/leetcode/middle/AddTwoNumbers.java
@@ -5,33 +5,38 @@ import org.leetcode.ListNode;
 /**
  * @Author: shiwei10
  * @Date: 2024/9/14 09:54
+ * @Description: 2. 两数相加
  */
 public class AddTwoNumbers {
     public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode dummy = new ListNode(-1, null);
+        ListNode dummy = new ListNode(-1);
         ListNode cur = dummy;
+
         int flag = 0;
+        int sum = 0;
+
         while (l1 != null || l2 != null) {
             int value1 = l1 == null ? 0 : l1.val;
             int value2 = l2 == null ? 0 : l2.val;
-            int sum = value1 + value2 + flag;
 
-            int value = sum % 10;
+            sum = value1 + value2 + flag;
+
             flag = sum / 10;
+            sum = sum % 10;
 
-            cur.next = new ListNode(value);
+            cur.next = new ListNode(sum);
             cur = cur.next;
 
-            if (l2 != null) {
-                l2 = l2.next;
+            if (l1 != null){
+                l1 = l1.next;
             }
 
-            if (l1 != null) {
-                l1 = l1.next;
+            if(l2 != null){
+                l2 = l2.next;
             }
         }
 
-        if (flag == 1) {
+        if(flag == 1){
             cur.next = new ListNode(flag);
         }
 
